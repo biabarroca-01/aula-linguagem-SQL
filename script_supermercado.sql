741d0f2bc808b1b0d524969b941609e6c4f95b10
biabarroca-01 <111541981+biabarroca-01@users.noreply.github.com>
2026-02-11 16:21:47 -0300

    Add files via upload

--- script_supermercado.sql
+++ script_supermercado.sql
@@ -1,20 +1,20 @@
---CREATE database supermercado;
+CREATE database supermercado;
 
---CREATE TABLE cliente(
---id int NOT NULL,
---nome varchar(50) NOT NULL,
---cpf varchar(11) NOT NULL,
---CONSTRAINT pk_id_cliente PRIMARY KEY (id)
---); 
+CREATE TABLE cliente(
+id int NOT NULL,
+nome varchar(50) NOT NULL,
+cpf varchar(11) NOT NULL,
+CONSTRAINT pk_id_cliente PRIMARY KEY (id)
+); 
 
---CREATE TABLE produto(
---id int NOT NULL,
---descricao varchar(100) NOT NULL,
---cod_barras varchar(14) NOT NULL,
---valor decimal (16,2)
---); 
+CREATE TABLE produto(
+id int NOT NULL,
+descricao varchar(100) NOT NULL,
+cod_barras varchar(14) NOT NULL,
+valor decimal (16,2)
+); 
 
---alter table produto add constraint pk_produto primary key (id);
---alter table produto add column situacao boolean not null;
---alter table produto alter column descricao type varchar (200);
---alter table produto drop column situacao;
+alter table produto add constraint pk_produto primary key (id);
+alter table produto add column situacao boolean not null;
+alter table produto alter column descricao type varchar (200);
+alter table produto drop column situacao;
